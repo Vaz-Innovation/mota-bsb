722c43b80fdfcf25d408588ac3e7a48450c9a015
Manuel Antunes <manuelnascimento5589@gmail.com>
2026-04-20 00:08:12 -0300

    fix: correct locales

--- codegen.ts
+++ codegen.ts
@@ -1,18 +1,15 @@
-import type { CodegenConfig } from '@graphql-codegen/cli';
+import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: [
-    process.env.WORDPRESS_API_URL ||
-      'http://localhost:8080/graphql',
-  ],
+  schema: [process.env.WORDPRESS_API_URL || "http://localhost:8080/graphql"],
   ignoreNoDocuments: true,
-  documents: ['src/**/*.{ts,tsx}'],
+  documents: ["src/**/*.{ts,tsx}"],
   generates: {
-    './src/graphql/__gen__/': {
-      preset: 'client',
+    "./src/graphql/__gen__/": {
+      preset: "client",
       plugins: [],
       config: {
-        documentMode: 'string',
+        documentMode: "string",
       },
     },
   },
