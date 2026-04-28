59ee5031717853af0864691759a9b9f4c7bb5f75
marcyroz <marcelly.nascimento84@gmail.com>
2026-04-28 16:44:02 -0300

    fix: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,16 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
 
 export default tseslint.config(
   { ignores: [".next", "node_modules", "src/graphql/__gen__"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: {
@@ -15,6 +19,7 @@ export default tseslint.config(
       },
     },
     rules: {
+      ...reactHooks.configs.recommended.rules,
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
